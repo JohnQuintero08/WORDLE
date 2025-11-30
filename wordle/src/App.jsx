f5064a4ba358f32b81928e4526dc787e30ec4650
John_Quintero <john08quinteroc@gmail.com>
2025-11-30 17:41:38 -0500

    Arregla el keyboard para que se vean los colores tambien

--- wordle/src/App.jsx
+++ wordle/src/App.jsx
@@ -63,7 +63,7 @@ export default function App() {
         wordLength={wordLength}
       />
 
-      <Keyboard onKeyPress={handleKeyPress} keyStatuses={{}} />
+      <Keyboard onKeyPress={handleKeyPress} keyStatuses={keyStatuses} />
     </div>
   );
 }
--- wordle/src/utils/updateKeyboardStatuses.js
+++ wordle/src/utils/updateKeyboardStatuses.js
@@ -1,13 +1,17 @@
+// src/utils/updateKeyboardStatuses.js
 export function updateKeyboardStatuses(prevStatuses, guess, statuses) {
   const priority = { correct: 3, present: 2, absent: 1 };
   const updated = { ...prevStatuses };
 
   guess.split("").forEach((letter, i) => {
     const newStatus = statuses[i];
-    const oldStatus = updated[letter];
+    if (!newStatus) return;
+
+    const key = letter.toUpperCase();
+    const oldStatus = updated[key];
 
     if (!oldStatus || priority[newStatus] > priority[oldStatus]) {
-      updated[letter] = newStatus;
+      updated[key] = newStatus;
     }
   });
 
